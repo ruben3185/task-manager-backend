e296a22e973496645a2a6f9ef82744102e44237d
ruben3185 <ruben3185@hotmail.com>
2025-05-10 06:50:38 -0500

    update corss

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
- 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');  // Importa el paquete cors
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const sequelize = require('./config/db');
@@ -8,6 +8,13 @@ const sequelize = require('./config/db');
 dotenv.config();
 
 const app = express();
+
+// Habilitar CORS con configuraciones específicas
+app.use(cors({
+  origin: 'http://localhost:5432',  // Permite peticiones solo desde este origen
+  credentials: true                // Si utilizas cookies o autenticación basada en credenciales
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
